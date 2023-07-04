de2341851dcdb744ad95d787d7394dd75cbab954
unknown <takumihikaru39@gmail.com>
2023-07-04 23:29:49 +0900

    解体新書6.1.3完了

--- src/main/java/com/example/applicatioln/service/UserApplicationService.java
+++ src/main/java/com/example/applicatioln/service/UserApplicationService.java
@@ -1,20 +1,28 @@
 package com.example.applicatioln.service;
 
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserApplicationService {
 
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	/**性別のMap生成する*/
-	public Map<String, Integer> getGenderMap(){
+	public Map<String, Integer> getGenderMap(Locale locale){
 		Map<String, Integer> genderMap = new LinkedHashMap<>();
+		String male = messageSource.getMessage("male", null,locale);
+		String female = messageSource.getMessage("female", null,locale);
 		
-		genderMap.put("男性",1);
-		genderMap.put("女性", 2);
+		genderMap.put(male,1);
+		genderMap.put(female, 2);
 		return genderMap;
 	}
 }
--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +24,9 @@ public class SignupController {
 	
 	/*ユーザー一覧画面を表示**/
 	@GetMapping("/signup")
-	public String getSignup(Model model) {
+	public String getSignup(Model model,Locale locale) {
 		//性別を取得
-		Map<String,Integer> genderMap = userApplicationService.getGenderMap();
+		Map<String,Integer> genderMap = userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
 		
 		
