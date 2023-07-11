f02c7e6eb9cc0e3729a0d33ee52a3f45c9df1c4d
unknown <takumihikaru39@gmail.com>
2023-07-12 00:23:50 +0900

    ユーザーマスタの全データを一覧画面に追加

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import java.util.Locale;
 import java.util.Map;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.applicatioln.service.UserApplicationService;
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
 import com.example.form.GroupOrder;
 import com.example.form.SignupForm;
 
@@ -32,7 +35,14 @@ public class SignupController {
 	private UserApplicationService userApplicationService;
 	
 	
-	/*ユーザー一覧画面を表示**/
+	@Autowired
+	private UserService userService;
+	
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	/*ユーザー登録画面を表示**/
 	@GetMapping("/signup")
 	public String getSignup(Model model,Locale locale,@ModelAttribute SignupForm form) {
 		//性別を取得
@@ -55,6 +65,14 @@ public class SignupController {
 		}
 		
 		log.info(form.toString());
+		
+		
+		//formをMUserクラスに変換
+		MUser user = modelMapper.map(form, MUser.class);
+		
+		
+		//ユーザー登録
+		userService.signup(user);
 		//ログイン画面にリダイレクト
 		return "redirect:/login";
 	}
