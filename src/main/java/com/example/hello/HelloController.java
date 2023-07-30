da76d5d0dfd6eaba1e7ccd46936d980deaa72313
unknown <takumihikaru39@gmail.com>
2023-07-30 22:13:18 +0900

    第3章2　別画面に値を渡す精読完了

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -9,42 +9,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller //ユーザーによるテキストボックスへの入力やボタン押下名ｄのイベントに対するハンドラとなり、該当のモデルのメソッドを呼び出す
 public class HelloController {
-	
+
 	@Autowired
 	private HelloService service;
 
-	@GetMapping("/hello")//URLの最後(/hello)http～/helloのhttpリクエストを受け取った場合行う処理
+	@GetMapping("/hello") //URLの最後(/hello)http～/hello　@GetMappingはメソッドとGETの処理を行うURLを紐づける役割を担います。
 	public String getHello() {
 		//hello.htmlに画面遷移
 		return "hello"; //正常に動いたら"hello" hello.htmlに遷移し画面を移す
 	}
 
+	/*@PostMappingはメソッドとPOSTの処理を行うURLを行うURLを紐付ける役割 
+	 *POSTはデータの登録、会員登録フォームから情報を入力してDBに登録する
+	*/
 	@PostMapping("/hello")
-	public String postRequest(@RequestParam("text1") String str, Model model) {
-
-		//画面から受け取った文字列をModelに登録
-		model.addAttribute("sample", str);
-
+	public String postRequest(@RequestParam("text1") String str,
+			Model model) { /*@RequestParam(text1)でhello.htmlのname=text1と一致するようにする
+							*と結びつき入力された値が@RequestParamに渡され変数strに格納される
+							//画面から受け取った文字列をModelに登録								     ModelクラスはControllerからView（画面）へ変数を渡すためのもの*/
+		model.addAttribute("sample", str);/*@RequestParam(text1)に渡された値をstrに渡し
+											キー名のsampleが呼ばれるとstrに格納されている値が出力される*/
 		//response.htmに画面遷移
 		return "hello/response";
 	}
-	
-	
+
 	@PostMapping("/hello/db")
-	public String postDbRequest(@RequestParam("text2") String id, Model model){
-		
-		
-		
+	public String postDbRequest(@RequestParam("text2") String id, Model model) {
+
 		//1件検索
-		
+
 		Employee employee = service.getEmployee(id);
-		
-		
+
 		//検索結果をModelに登録
-		
+
 		model.addAttribute("employee", employee);
-		
-		
+
 		//db.htmlに画面を遷移
 		return "hello/db";
 	}
