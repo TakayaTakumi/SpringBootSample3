5a21fd6506d8430e7c6fea57f43018c6a980fb2d
unknown <takumihikaru39@gmail.com>
2023-08-16 23:54:42 +0900

    解体新書　コメント　8.2.2完了

--- src/main/java/com/example/controller/UserListController.java
+++ src/main/java/com/example/controller/UserListController.java
@@ -34,6 +34,11 @@ public class UserListController {
 	@GetMapping("/list")
 	public String getUserList(@ModelAttribute UserListForm form,Model model){
 		
+		/*1.、mapメソッドを用いてフォームの内容から第二引数（今回はMUserクラス）のインスタンス(user)を新しく生成。
+		 *2.userの各インスタンスフィールドについて、同名のものが第一引数（今回であればform）に存在するかどうかをチェック。
+		 *3.もし存在していれば、formからuserに値をコピーする。
+		 *  ちなみに、この処理は内部的にはゲッターとセッターを用いて行っているそうなので、
+		 *  あらかじめ両者(formとuser)にゲッターとセッターが定義されている必要があります。*/
 		//formをMUserクラスに変換
 		MUser user = modelMapper.map(form, MUser.class);
 		/*UserService.javaからユーザーの一覧を取得している*/
@@ -41,7 +46,7 @@ public class UserListController {
 		List<MUser>userList = userService.getUsers(user);
 		/*List<MUser>userListを("キー名",変数)で登録している　引数にModel modelを登録して言うr*/
 		//Modelに登録
-		model.addAttribute("userList",userList);
+		model.addAttribute("userList",userList);//コントローラからビューにデータを渡すために使用
 		
 		//ユーザー一覧画面を表示
 		return "user/list";
